e90ab4ca3c5d10d8876c427895fbe30aa55807f3
enter-the-code <ssoonngg53@naver.com>
2022-11-17 02:41:06 +0900

    백엔드 swagger 구축 및 보안

--- back/src/auth/controller/controller.controller.ts
+++ back/src/auth/controller/controller.controller.ts
@@ -1,11 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 import { User } from '@prisma/client';
 import { ServiceService } from '../service/service.service';
+import { ApiOperation } from '@nestjs/swagger';
 
 @Controller('auth')
 export class ControllerController {
   constructor(private readonly Service: ServiceService) {}
 
+  @ApiOperation({ summary: '로그인' })
   @Get()
   async fetchAllUser(): Promise<User[]> {
     return this.Service.takeOneUser();
--- back/src/main.ts
+++ back/src/main.ts
@@ -1,10 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './http-exception.filter';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import * as expressBasicAuth from 'express-basic-auth';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new HttpExceptionFilter());
+  app.use(
+    ['/docs', 'docs-json'],
+    expressBasicAuth({
+      challenge: true,
+      users: {
+        [process.env.SWAGGER_USER]: process.env.SWAGGER_PASSWORD,
+      },
+    }),
+  );
+  const config = new DocumentBuilder()
+    .setTitle('한마음 교회 공식 api')
+    .setDescription('api문서')
+    .setVersion('1.0.0')
+    .addTag('onemind')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
   await app.listen(3000);
 }
 bootstrap();
