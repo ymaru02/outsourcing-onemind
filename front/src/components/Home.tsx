c3260f81adb4897b5f34414c448411939f60eacc
Ymaru02 <dyun022@gmail.com>
2022-12-01 21:49:54 +0900

    merge: merge

--- front/src/components/Home.tsx
+++ front/src/components/Home.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import styled from 'styled-components';
 
 export default function Home() {
   return (
-    <div>Home</div>
-  )
+    <>
+    <video controls width={'100%'} autoPlay={true}>
+        <source src="/background.mp4"type="video/mp4"/>
+    </video>
+    </>
+    )
 }
