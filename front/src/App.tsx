becbe3d3d55755c7e708aee7adc3f6cdab32a62d
Ymaru02 <dyun022@gmail.com>
2022-12-03 23:03:15 +0900

    feat: main Layout

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,16 +1,18 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NotFound from "./components/NotFound";
-import Home from "./components/Home";
+import Home from "./components/Layout/Home";
 import Footer from "./components/Footer";
 import Introduction from "./components/Deng/Introduction";
 import Location from "./components/Deng/Location";
 import GStyle from "./styles/GStyle"; // GlobalStyle
+import Header from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <BrowserRouter>
       <GStyle /> {/* GlobalStyle */}
+      <Header></Header>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route element={<NotFound />} />
--- front/src/components/Footer.jsx
+++ front/src/components/Footer.jsx
@@ -1,19 +1,42 @@
-import "./CSS/Footer.css"
+import React from "react";
+import styled from "styled-components";
 
-const Footer = () => {
-    return(
-        <>
-        <div class="wrap">
-            <div class="Footer">
-                <ul>
-                    <li><a href="">위치: 대구광역시 수성구 범안8길 19</a></li>
-                    <li><a href="">목사님: 서정현 목사님</a></li>
-                    <li><a href="">전화번호(Tel): 052 - xxxx - xxxx</a></li>
-                </ul>
-                <div>Copyright ⓒ 2022 한마음교회 All Rights reserved.</div>
-            </div>
-        </div>
-        </>
-    )
+const Wrap = styled.div`
+  margin: 30px 0 0;
+  padding: 0px;
+  width: 100%;
+  height: 100%;
+`;
+
+const Inner = styled.div`
+  width: 100%;
+  height: 100%;
+  text-align: center;
+  font-size: 1rem;
+  font-weight: bold;
+`;
+
+const Item = styled.li`
+  text-decoration: none;
+  list-style-type: none;
+  display: inline;
+  padding: 20px;
+`;
+const ItemText = styled.div`
+  padding: 20px;
+`;
+
+export default function Footer() {
+  return (
+    <Wrap>
+      <Inner>
+        <ul>
+          <Item>위치: 대구광역시 수성구 범안8길 19</Item>
+          <Item>목사님: 서정현 목사님</Item>
+          <Item>전화번호(Tel): 052 - xxxx - xxxx</Item>
+        </ul>
+        <ItemText>Copyright ⓒ 2022 한마음교회 All Rights reserved.</ItemText>
+      </Inner>
+    </Wrap>
+  );
 }
-export default Footer
