5c16c1836dc5b7bc3c038918abf006072254ceb5
Deng Lee <edaero22@naver.com>
2022-12-04 19:20:35 +0900

    add menu

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -4,6 +4,8 @@ import NotFound from "./components/NotFound";
 import Home from "./components/Layout/Home";
 import Footer from "./components/Footer";
 import Introduction from "./components/Layout/Introduction";
+import News from "./components/Layout/News";
+import Album from "./components/Layout/Album";
 import Location from "./components/Layout/Location";
 import Worship from "./components/Layout/Worship";
 import Navbar from "./components/Navbar/Navbar";
@@ -16,6 +18,8 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route element={<NotFound />} />
         <Route path="/introduction" element={<Introduction />} />
+        <Route path="/news" element={<News />} />
+        <Route path="/album" element={<Album />} />
         <Route path="/location" element={<Location />} />
         <Route path="/worship" element={<Worship />} />
       </Routes>
---
+++ front/src/components/Layout/Album.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "aos/dist/aos.css";
+import {
+  Wrap,
+  ImgBox,
+  DivideBox,
+  Img,
+  ContentsBox,
+  InfoTitleDiv,
+} from "../../styles/Intro";
+import Sidebar from "../Sidebar/Sidebar";
+import Rainbow250 from "../../img/rainbowVer250.png";
+
+export default function Album() {
+  return (
+    <Wrap>
+      <ImgBox>
+        <Img src={Rainbow250} alt="" width="100%" height="100%" margin="auto" />
+      </ImgBox>
+      <DivideBox>
+        <Sidebar />
+        <ContentsBox>
+          <InfoTitleDiv fontsize="20px">교회 앨범</InfoTitleDiv>
+        </ContentsBox>
+      </DivideBox>
+    </Wrap>
+  );
+}
