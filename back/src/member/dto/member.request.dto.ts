c79384e7881ba00591e1bebd0fd010c841ea814c
enter-the-code <ssoonngg53@naver.com>
2022-12-06 03:46:59 +0900

    디비수정

--- back/src/member/dto/member.request.dto.ts
+++ back/src/member/dto/member.request.dto.ts
@@ -12,15 +12,6 @@ export class MemberDto {
   name: string;
 
   @ApiProperty({
-    example: '간단한 1줄짜리 설명',
-    description: '없어도 되긴함 나이 몇세 장로직 이런식으로',
-    required: true,
-  })
-  @IsString()
-  @IsNotEmpty()
-  explain: string;
-
-  @ApiProperty({
     example: '직책',
     description: '장로',
     required: true,
