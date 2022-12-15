1c4ec2915adb65d09facd2d58782b9045efd2d5a
Deng Lee <edaero22@naver.com>
2022-12-15 17:42:31 +0900

    PasterItem

--- front/src/components/Layout/Pastor.tsx
+++ front/src/components/Layout/Pastor.tsx
@@ -12,12 +12,12 @@ import {
   Img,
   TinyTitle,
   PastorDiv,
-  PastorItem,
   TinyTitles,
 } from "../../styles/Intro";
 import Sidebar from "../Sidebar/Sidebar";
 import Rainbow250 from "../../img/rainbowVer250.png";
 import pastor from "../../img/pastor.jpg";
+import PasterItem from "../Pastor/PasterItem";
 
 export default function Pastor() {
   useEffect(() => {
@@ -43,96 +43,49 @@ export default function Pastor() {
               <PastorDiv>
                 <div>
                   <TinyTitle fontsize="22px">담임 목사</TinyTitle>
-                  <PastorItem>
-                    <img src={pastor} alt="" width="100%" height="100%" />
-                    <p>서정현</p>
-                    <p>010-5739-0916</p>
-                  </PastorItem>
+                  <PasterItem name="서정현" phone="010-5739-0916" />
                 </div>
               </PastorDiv>
               <PastorDiv>
                 <div>
                   <TinyTitle fontsize="22px">협동 목사</TinyTitle>
-                  <PastorItem>
-                    <img src={pastor} alt="" width="100%" height="100%" />
-                    <p>김병철</p>
-                    <p>010-2688-1759</p>
-                  </PastorItem>
+                  <PasterItem name="김병철" phone="010-2688-1759" />
                 </div>
                 <div>
                   <TinyTitle fontsize="22px">교육 목사</TinyTitle>
-                  <PastorItem>
-                    <img src={pastor} alt="" width="100%" height="100%" />
-                    <p>김갑수</p>
-                    <p>010-8595-4952</p>
-                  </PastorItem>
+                  <PasterItem name="김갑수" phone="010-8595-4952" />
                 </div>
                 <div>
                   <TinyTitle fontsize="22px">사역 목사</TinyTitle>
-                  <PastorItem>
-                    <img src={pastor} alt="" width="100%" height="100%" />
-                    <p>임인환</p>
-                    <p>010-8565-0681</p>
-                  </PastorItem>
+
+                  <PasterItem name="임인환" phone="010-8565-0681" />
                 </div>
               </PastorDiv>
               <TinyTitles fontsize="22px">시무 장로</TinyTitles>
               <PastorDiv>
-                <PastorItem>
-                  <img src={pastor} alt="" width="100%" height="100%" />
-                  <p>최성철</p>
-                  <p>010-4160-9898</p>
-                </PastorItem>
-                <PastorItem>
-                  <img src={pastor} alt="" width="100%" height="100%" />
-                  <p>이강환</p>
-                  <p>010-9290-4040</p>
-                </PastorItem>
+                <PasterItem name="최성철" phone="010-4160-9898" />
+                <PasterItem name="이강환" phone="010-9290-4040" />
               </PastorDiv>
               <PastorDiv>
                 <div>
                   <TinyTitle fontsize="22px">전도사</TinyTitle>
-                  <PastorItem>
-                    <img src={pastor} alt="" width="100%" height="100%" />
-                    <p>정옥숙</p>
-                    <p>010-5556-7651</p>
-                  </PastorItem>
+
+                  <PasterItem name="정옥숙" phone="010-5556-7651" />
                 </div>
                 <div>
                   <TinyTitle fontsize="22px">영아부</TinyTitle>
-                  <PastorItem>
-                    <img src={pastor} alt="" width="100%" height="100%" />
-                    <p>손상희</p>
-                    <p>010-2629-1757</p>
-                  </PastorItem>
+
+                  <PasterItem name="손상희" phone="010-2629-1757" />
                 </div>
               </PastorDiv>
               <TinyTitles fontsize="22px">시무 권사</TinyTitles>
               <PastorDiv>
-                <PastorItem>
-                  <img src={pastor} alt="" width="100%" height="100%" />
-                  <p>장영례</p>
-                </PastorItem>
-                <PastorItem>
-                  <img src={pastor} alt="" width="100%" height="100%" />
-                  <p>장지양</p>
-                </PastorItem>
-                <PastorItem>
-                  <img src={pastor} alt="" width="100%" height="100%" />
-                  <p>최경희</p>
-                </PastorItem>
-                <PastorItem>
-                  <img src={pastor} alt="" width="100%" height="100%" />
-                  <p>한순남</p>
-                </PastorItem>
-                <PastorItem>
-                  <img src={pastor} alt="" width="100%" height="100%" />
-                  <p>안현숙</p>
-                </PastorItem>
-                <PastorItem>
-                  <img src={pastor} alt="" width="100%" height="100%" />
-                  <p>성순정</p>
-                </PastorItem>
+                <PasterItem name="장영례" />
+                <PasterItem name="장지양" />
+                <PasterItem name="최경희" />
+                <PasterItem name="한순남" />
+                <PasterItem name="안현숙" />
+                <PasterItem name="성순정" />
               </PastorDiv>
             </InfoDiv>
           </ContentsDiv>
---
+++ front/src/components/Pastor/PasterItem.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { PastorItem } from "../../styles/Intro";
+import pastor from "../../img/pastor.jpg";
+
+interface PasterItem {
+  name: string;
+  phone?: string;
+}
+
+export default function PasterItem({ name, phone }: PasterItem) {
+  return (
+    <PastorItem>
+      <img src={pastor} alt="" width="100%" height="100%" />
+      <p>{name}</p>
+      <p>{phone}</p>
+    </PastorItem>
+  );
+}
